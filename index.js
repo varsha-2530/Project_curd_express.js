7c2b59a0f6e7a045305ae4ad766ac92ce7deb472
ADMIN <admin@ADMINs-MacBook-Pro.local>
2025-05-16 22:55:39 +0530

    showuser by id

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ app.use(express.json());
 
 app.get('/showAllUser', (request, response) => {
     response.status(200).json(BlogUser);
-})
+});
+
+app.get('/showUser/:UserId', (request, response)=>{
+        const id = parseInt(request.params.UserId);
+        let user = BlogUser.filter(BlogUsers => BlogUsers.id === id);
+
+        (user.length !== 0) ? response.status(200).json(user) 
+        : response.status(404).json({ message: `User not found with id ${id}` })
+});
 
 app.listen(PORT, () => {
     console.log(`server is runing on http://localhost:${PORT}`);
 
-})
+});
